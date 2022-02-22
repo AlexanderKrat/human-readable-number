44b735ce9bf65e3a4bf56a40500b0b058c5c35f9
Alexander <alexanderkratexor@gmail.com>
2022-02-22 23:29:53 +0200

    docs: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function reverse (n) {
+    let reverse = n.toString().split('').reverse().join('');
+    if (n < 0){
+        return reverse.slice(0, -1)
+    }
+    else{
+        return reverse
+    }
 }
