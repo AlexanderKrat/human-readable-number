7a120249fe76785d6087911c421cbe5f045ecdbd
AlexanderKrat <95974403+AlexanderKrat@users.noreply.github.com>
2022-02-25 00:32:06 +0200

    docs: update file

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ module.exports = function toReadable (number) {
     ];
     let index1 = numberarr[0] - 1;
     let index2 = numberarr[1] - 1;
-    let index3 = numberarr[2];
+    let index3 = numberarr[2] - 1;
 
 if(number === 0){
     return "zero";
@@ -21,13 +21,18 @@ if(number % 10 === 0 && number < 100){
     return tens[index1];
 }
 else if(number < 20){
-return from0To19[number];
+return from0To19[number - 1];
 }
 else if(number > 19 && number < 100){
 return tens[index1] + " " + from0To19[index2];
 }
-else if (number ){
-    return from0To19[index1 + 1] + " hundred"
+if(number % 100 === 0 && number > 99){
+    return from0To19[index1] + ' hundred'
 }
-
+else if(number > 99 && number % 10 === 0){
+    return from0To19[index1] + ' hundred' + " " + tens[index2]
+}
+else if(number > 99 && number % 10 > 0){
+    return from0To19[index1] + ' hundred' + " " + tens[index2] + " " + from0To19[index3]
+}
 }
